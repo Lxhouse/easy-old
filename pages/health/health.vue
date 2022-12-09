bddd458d084c3f6de4a850b0cd73deb4213cd8d1
zhemu <lingx@ibuscloud.com>
2022-12-09 12:56:52 +0800

    feat: 优化打卡按钮

--- pages/health/health.vue
+++ pages/health/health.vue
@@ -15,7 +15,7 @@
 			</u-grid>
 		</view>
 		<view class="clockIn">
-			<button class="clockBtn">打卡</button>
+			<button class="clockBtn succeed">{{clockBtnText}}</button>
 		</view>
 	</view>
 </template>
@@ -24,6 +24,7 @@
 	export default {
 		data() {
 			return {
+				clockBtnText:"打卡",
 				moreFun: [{
 						icon: '/static/img/time.png',
 						word: '打卡信息',
@@ -56,21 +57,19 @@
 					uni.navigateTo({
 						url: '/pages/signIn/signIn'
 					})
-				}else if(e==='goCashCouponList'){
+				} else if (e === 'goCashCouponList') {
 					uni.navigateTo({
-						url:'/pages/charts/charts'
+						url: '/pages/charts/charts'
 					})
-				}
-				else if(e==='onHelp'){
+				} else if (e === 'onHelp') {
 					uni.navigateTo({
-						url:'/pages/drugs/drugs'
+						url: '/pages/drugs/drugs'
 					})
 				}
 			},
 		}
 	}
 </script>
-
 <style lang="scss">
 	.health_warp {
 
@@ -114,11 +113,61 @@
 			display: flex;
 			align-items: center;
 			justify-content: center;
-			.clockBtn{
+
+			.clockBtn {
 				width: 400rpx;
 				height: 400rpx;
-box-sizing: border-box;
+				margin-top: 20%;
+				border-radius: 50%;
 				text-align: center;
+				text-align: center;
+				line-height: 400rpx;
+				font-size: 60rpx;
+				cursor: pointer;
+				// border: 1px solid #fff;
+
+			}
+
+			.succeed {
+				color: #fff;
+				background-color: $uni-color-success;
+				box-shadow: 0 1px 2px rgba(0, 0, 0, .3);
+				/*线性渐变语法：
+	    background-image:-webkit-gradient(type,x1 y1, x2 y2, from(开始颜色值),to(结束颜色值), [color-stop(偏移量小数,停靠颜色值),…] );
+	        type:line   -->表示渐变为线性
+	        x1,x2,y1,y2     -->渐变坐标，x1=x2垂直变换,y1=y2左右变换
+	    */
+				background-image: -webkit-gradient(linear, left top, left bottom, from(#33CC66), to($uni-color-success));
+				/*上下垂直渐变*/
+
+				/*规定动画变换速度：ease-in-out -->动画以低速开始和结束 */
+				-webkit-animation-timing-function: ease-in-out;
+
+				/*为 @keyframes 动画规定一个名称*/
+				-webkit-animation-name: submit;
+
+				/*完成一个动画所需时间*/
+				-webkit-animation-duration: 2500ms;
+
+				/*规定动画播放次数：infinite为无限次*/
+				-webkit-animation-iteration-count: infinite;
+
+				/*规定动画播放方式：alternate为轮流反向播放*/
+				-webkit-animation-direction: alternate;
+			}
+
+			@-webkit-keyframes submit {
+				0% {
+					opacity: .5;
+					/*元素不透明级别*/
+					box-shadow: 0 1px 2px rgba(255, 255, 255, 0.1);
+				}
+
+				100% {
+					opacity: 1;
+					// border: 1px solid rgba(51, 153, 0, 2);
+					box-shadow: 0 1px 30px rgba(51, 153, 0, 2);
+				}
 			}
 		}
 	}
